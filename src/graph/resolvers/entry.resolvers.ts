d56b9c8a41c405f582aeef89210cb70c2d5ec8f6
Noah Milberger <n.milberger@gmail.com>
2022-12-30 22:53:10 -0500

    Delete old resolver.

--- src/graph/resolvers/entry.resolvers.ts
+++
@@ -1,8 +0,0 @@
-const { prisma } = require("../../prisma/client");
-import { EntryResolvers } from "../../__generated__/resolvers-types";
-
-export const entryResolvers: EntryResolvers = {
-  entries: () => {
-    return prisma.entry.findMany();
-  },
-};
